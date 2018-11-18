1ec5b4b493039fa62ec97bda3266b858f423393e
Christopher Ragasa <ragasac@os1.engr.oregonstate.edu>
2018-11-17 22:22:12 -0800

    initial commit

---
+++ keygen.c
@@ -0,0 +1,10 @@
+/*******************************
+Christopher Ragasa
+OSU - CS 344, Program 4 (OTP)
+Program Description: 
+
+OTP is a system that will encypt and decrypt information using a one-time pad-like system.
+This specific program is 1 of the 5 small that make up the entire system. Keygen creates
+a key file of specified length. The characters in the file generated will be any of the 27
+allowed characters, generated using the standard UNIX randomization methods.  
+********************************/
